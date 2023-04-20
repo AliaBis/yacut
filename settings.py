45b9ae8436112bdd0b88eb259bcf0e6793e76570
Алия Бисенгалиева <105454189+AliaBis@users.noreply.github.com>
2023-04-21 00:05:55 +0300

    settings

--- settings.py
+++ settings.py
@@ -1,7 +1,10 @@
 import os
 
+ALLOWED_CHARACTERS = r'^[A-Za-z0-9]{1,16}$'
+FORBIDDEN_CHARACTERS = r'^[a-z]+://[^\/\?:]+(:[0-9]+)?(\/.*?)?(\?.*)?$'
+LENGTH = 6
 
-class Config(object):
+class Config():
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
