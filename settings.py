fa8c2b6182f509b6f338439051ec4ce742069d33
Алия Бисенгалиева <105454189+AliaBis@users.noreply.github.com>
2023-04-21 00:09:08 +0300

    flake8 settings

--- settings.py
+++ settings.py
@@ -4,6 +4,7 @@ ALLOWED_CHARACTERS = r'^[A-Za-z0-9]{1,16}$'
 FORBIDDEN_CHARACTERS = r'^[a-z]+://[^\/\?:]+(:[0-9]+)?(\/.*?)?(\?.*)?$'
 LENGTH = 6
 
+
 class Config():
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
