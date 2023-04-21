add607e9693f2d484774ebbb84376b004c71e281
Алия Бисенгалиева <105454189+AliaBis@users.noreply.github.com>
2023-04-21 15:16:53 +0300

    проверка __init__

--- settings.py
+++ settings.py
@@ -7,6 +7,6 @@ RANDOM_LINK_LENGTH = 6
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', default='sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', default='SECRET_KEY')
